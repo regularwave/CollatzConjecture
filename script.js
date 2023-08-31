e0ca5ba15f203f01c674f1605a02af3047f35bc0
regularwave <regularwave@gmail.com>
2023-08-30 21:01:28 -0400

    clears sequences in between

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ var sequenceOutput = document.getElementById('sequence');
 
 function calcsequence() {
     var startSeed = document.getElementById('startseed').value;
+    sequenceOutput.innerHTML = '';
 
     do {
-        sequenceOutput.innerHTML += startSeed + ", "
+        sequenceOutput.innerHTML += startSeed + ", ";
         if (startSeed % 2 == 0) {
-            startSeed = startSeed / 2
+            startSeed = startSeed / 2;
         } else {
-            startSeed = startSeed * 3 + 1
+            startSeed = startSeed * 3 + 1;
         }
     } while (startSeed > 1)
 
-    sequenceOutput.innerHTML += startSeed
+    sequenceOutput.innerHTML += startSeed;
 }
